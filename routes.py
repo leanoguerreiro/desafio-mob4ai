c57c922e5ecd4a0de78e537486f1c706c5c8c226
leanoguerreiro <leanincontact@gmail.com>
2025-08-14 12:27:05 -0400

    Add schemas for Metrics and Process management using Pydantic

--- routes.py
+++ routes.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from database.database import get_db
-from database.models import Processes1
+from database.models import Processes1, Processes2, Processes3
+
 
 from utils.parse_metrics import parse_metrics
 
@@ -17,12 +18,24 @@ process_router = APIRouter(prefix="/process", tags=["process"])
 
 @process_router.get("/processes", response_model=list[ProcessSchema])
 async def get_processes(db: Session = Depends(get_db)):
+    
+    results = (
+        db.query(Processes1, Processes2, Processes3)
+        .join(Table2, process)
+    )
     processes = db.query(Processes1).all()
     return [
         {
             "PackageName": process.PackageName,
             "Uid": process.Uid,
-            "Metrics": parse_metrics(process.Metrics)  # Converte a string para um dicionário
+            "Metrics": parse_metrics(process.Metrics) or {
+                "timestamp": 0,  # Valor padrão
+                "usagetime": 0,  # Valor padrão
+                "delta_cpu_time": 0,
+                "cpu_usage": 0.0,
+                "rx_data": 0,
+                "tx_data": 0,
+            }        
         }
         for process in processes
     ]
