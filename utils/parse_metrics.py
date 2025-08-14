f55d2dbddeaa7bec345d538d063b385e557dd0ff
leanoguerreiro <leanincontact@gmail.com>
2025-08-14 10:57:59 -0400

    Add metrics parsing function to parse_metrics.py

---
+++ utils/parse_metrics.py
@@ -0,0 +1,12 @@
+def parse_metrics(metrics: str):
+    keys = [
+        'timestamp',
+        'usagetime',
+        'delta_cpu_time',
+        'cpu_usage',
+        'rx_data',
+        'tx_data'
+    ]
+    values = metrics.split(':')
+    parsed = {keys[i]: float(values[i]) if "." in values[i] else int(values[i]) for i in range(len(keys))}
+    return parsed
